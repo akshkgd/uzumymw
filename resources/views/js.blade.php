eebdf62f58dd9867f85a261c95e37977138a3ccb
Ashish Shukla <akshkgd@gmail.com>
2021-08-20 23:31:17 +0530

    fix

--- resources/views/js.blade.php
+++ resources/views/js.blade.php
@@ -47,8 +47,7 @@
                 <h1 class="title">Get started with 3 days  <span class="wd_highlight">Live Javascript</span> <br> Bootcamp at 95% off</h1>
                
                 <div class="row justify-content-center">
-                    <p class="fs-5 text-muted col-md-10 text-center">Get started as a front-end web developer using HTML, CSS,
-                        JavaScript and Bootstrap. <br> The modern web development course for everyone! </p>
+                    <p class="fs-5 text-muted col-md-10 text-center">Modern JavaScript from the beginning - all the way up to JS expert level! THE must-have JavaScript masterclass in 2021. </p>
                 </div>
 
 
