e33da1190183fc68a4d37891f5c22e5e984c578d
Ashish Shukla <akshkgd@gmail.com>
2021-11-16 19:38:53 +0530

    wdm

--- resources/views/wdm.blade.php
+++ resources/views/wdm.blade.php
@@ -414,20 +414,15 @@
         minute = second * 60,
         hour = minute * 60,
         day = hour * 24;
-        let minutesToAdd= 30;
-        console.log(minutesToAdd)
-        let currentDate = new Date();
-        // let futureDate = new Date(currentDate.getTime() + minutesToAdd*60000);
-    
-    // let birthday = futureDate,
-    // countDown = new Date(birthday).getTime(),
-    x = setInterval(function() {    
-        let birthday = "Nov 16, 2021 24:00:00",
-        countDown = new Date(birthday).getTime(),
-        let now = new Date().getTime(),
-        distance = countDown - now;
 
-          document.getElementById("days").innerText = Math.floor(distance / (day)),
+  let birthday = "Nov 16, 2021 24:00:00",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        let now = new Date().getTime(),
+            distance = countDown - now;
+
+        document.getElementById("days").innerText = Math.floor(distance / (day)),
           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
           document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
