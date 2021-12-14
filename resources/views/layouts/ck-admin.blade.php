ea6e048755c94853bb1666be5374c5909821a9d5
Ashish Shukla <akshkgd@gmail.com>
2021-12-14 11:55:27 +0530

    bug fix

--- resources/views/layouts/ck-admin.blade.php
+++ resources/views/layouts/ck-admin.blade.php
@@ -37,7 +37,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white">
       <div class="container">
-        <a class="navbar-brand text-primary fw-bolder fs-5" href="#">
+        <a class="navbar-brand text-primary fw-bolder fs-5" href="{{url('/home')}}">
             Codekaro
         </a>
         <button
@@ -63,15 +63,17 @@
               <a class="nav-link" href="#">Link</a>
             </li>
 
-            <li class="nav-item">
-              <a
-                class="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-                >Link</a
-              >
-            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
           </ul>
           <ul class="navbar-nav">
             <li class="dropdown ms-2">
