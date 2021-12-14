8da8157405784cda4aa6a3c27b333c2dc16ab203
Ashish Shukla <akshkgd@gmail.com>
2021-12-14 09:57:38 +0530

    bug fix

--- resources/views/admin/batchEnrollment.blade.php
+++ resources/views/admin/batchEnrollment.blade.php
@@ -1,6 +1,50 @@
 @extends('layouts.ck-admin')
 @section('content')
-    
+<div class="container mt-5">
+    <div class="row">
+        <div class="card card-body">
+
+            <h4 class="">{{$batch->name}}</h1>
+        <div class="col-6 col-md">
+            <div class="stat-cell stat-cell-red p-2">
+                <p class="stat-cell-title">Total Users</p>
+                <p class="stat-cell-value">{{ $users }}</p>
+            </div>
+
+        </div>
+        <div class="col-6 col-md">
+            <div class="stat-cell stat-cell-purple p-2">
+                <p class="stat-cell-title">Users (This Month)</p>
+                <p class="stat-cell-value">{{ $batches }}</p>
+            </div>
+
+        </div>
+        <div class="col-6 col-md">
+            <div class="stat-cell stat-cell-teal p-2">
+                <p class="stat-cell-title">Live Batches</p>
+                <p class="stat-cell-value">{{ $batches }}</p>
+            </div>
+
+        </div>
+        <div class="col-6 col-md">
+            <div class="stat-cell stat-cell-green p-2">
+                <p class="stat-cell-title">Users</p>
+                <p class="stat-cell-value">4343</p>
+            </div>
+
+        </div>
+        <div class="col-6 col-md">
+            <div class="stat-cell stat-cell-yellow p-2">
+                <p class="stat-cell-title">Users</p>
+                <p class="stat-cell-value">4343</p>
+            </div>
+
+        </div>
+    </div>
+</div>
+</div>
+
+
 
     {{-- Enrollment details --}}
 
@@ -9,11 +53,11 @@
             @include('layouts.alert')
             <div class="row justify-content-center">
                 <div class="col-lg-12 col-xl-12 col-sm-12 col-md-12 ">
-                    <h4 class="">{{$batch->name}}</h1>
+                    
                     <div class="card mb-5">
                         <div class="card-boy">
                             <div class="p-3">
-                                <h3 class="ck-font">Students Enrolled</h3>
+                                <h3 class="fs-5 text-dark">Students Enrolled</h3>
                             </div>
                             <div class="table-responsive">
                             <table class="table table-responsive">
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,83 +5,83 @@
     <!-- main dashboard -->
 
     <div class="container mt-5">
-      <div class="row">
-          <div class="col-6 col-md">
-              <div class="stat-cell stat-cell-red p-2">
-                      <p class="stat-cell-title">Total Users</p>
-                      <p class="stat-cell-value">{{$users}}</p>
-                  </div>
-                  
-              </div>
-              <div class="col-6 col-md">
-                  <div class="stat-cell stat-cell-purple p-2">
-                          <p class="stat-cell-title">Users (This Month)</p>
-                          <p class="stat-cell-value">{{$batches}}</p>
-                      </div>
-                      
-                  </div>
-                  <div class="col-6 col-md">
-                      <div class="stat-cell stat-cell-teal p-2">
-                              <p class="stat-cell-title">Live Batches</p>
-                              <p class="stat-cell-value">{{$batches}}</p>
-                          </div>
-                          
-                      </div>
-                      <div class="col-6 col-md">
-                          <div class="stat-cell stat-cell-green p-2">
-                                  <p class="stat-cell-title">Users</p>
-                                  <p class="stat-cell-value">4343</p>
-                              </div>
-                              
-                          </div>
-                          <div class="col-6 col-md">
-                              <div class="stat-cell stat-cell-yellow p-2">
-                                      <p class="stat-cell-title">Users</p>
-                                      <p class="stat-cell-value">4343</p>
-                                  </div>
-                                  
-                              </div>
-          </div>
-      </div>
-  
-  
-  
-  
-      <!-- main dashboard ends -->
+        <div class="row">
+            <div class="col-6 col-md">
+                <div class="stat-cell stat-cell-red p-2">
+                    <p class="stat-cell-title">Total Users</p>
+                    <p class="stat-cell-value">{{ $users }}</p>
+                </div>
 
-<!-- First row (equally spaced) -->
-<div class="row row-eq-spacing d-none">
-    <div class="col-6 col-xl-3">
-      <div class="card">
-        <h2 class="card-title">Users</h2>
-        <h4>{{$users}}</h3>
-          <a href="{{url('/admin/students')}}" class="btn btn-outline-primary d-block">Students</a>
-      </div>
+            </div>
+            <div class="col-6 col-md">
+                <div class="stat-cell stat-cell-purple p-2">
+                    <p class="stat-cell-title">Users (This Month)</p>
+                    <p class="stat-cell-value">{{ $batches }}</p>
+                </div>
+
+            </div>
+            <div class="col-6 col-md">
+                <div class="stat-cell stat-cell-teal p-2">
+                    <p class="stat-cell-title">Live Batches</p>
+                    <p class="stat-cell-value">{{ $batches }}</p>
+                </div>
+
+            </div>
+            <div class="col-6 col-md">
+                <div class="stat-cell stat-cell-green p-2">
+                    <p class="stat-cell-title">Users</p>
+                    <p class="stat-cell-value">4343</p>
+                </div>
+
+            </div>
+            <div class="col-6 col-md">
+                <div class="stat-cell stat-cell-yellow p-2">
+                    <p class="stat-cell-title">Users</p>
+                    <p class="stat-cell-value">4343</p>
+                </div>
+
+            </div>
+        </div>
     </div>
-    <div class="col-6 col-xl-3">
-      <div class="card">
-        <h2 class="card-title">Live Batches</h2>
-        <h4>{{$batches}}</h3>
-          <a href="{{url('/admin/batches')}}" class="btn btn-outline-primary d-block"> </i>Details</a>
-      </div>
+
+
+
+
+    <!-- main dashboard ends -->
+
+    <!-- First row (equally spaced) -->
+    <div class="row row-eq-spacing d-none">
+        <div class="col-6 col-xl-3">
+            <div class="card">
+                <h2 class="card-title">Users</h2>
+                <h4>{{ $users }}</h3>
+                    <a href="{{ url('/admin/students') }}" class="btn btn-outline-primary d-block">Students</a>
+            </div>
+        </div>
+        <div class="col-6 col-xl-3">
+            <div class="card">
+                <h2 class="card-title">Live Batches</h2>
+                <h4>{{ $batches }}</h3>
+                    <a href="{{ url('/admin/batches') }}" class="btn btn-outline-primary d-block"> </i>Details</a>
+            </div>
+        </div>
+        <!-- Overflow occurs here on large screens (and down) -->
+        <!-- Therefore, a v-spacer is added at this point -->
+        <div class="v-spacer d-xl-none"></div> <!-- d-xl-none = display: none only on extra large screens (> 1200px) -->
+        <div class="col-6 col-xl-3">
+            <div class="card">
+                <h2 class="card-title">Users</h2>
+                <h4>345</h3>
+
+                    <a href="{{ url('/user') }}" class="btn btn-outline-primary d-block"> </i>Details</a>
+            </div>
+        </div>
+        <div class="col-6 col-xl-3">
+            <div class="card">
+                <h2 class="card-title">Suppliers</h2>
+                <h4>1</h3>
+                    <a href="{{ url('/supplier') }}" class="btn btn-outline-primary d-block"> </i>Details</a>
+            </div>
+        </div>
     </div>
-    <!-- Overflow occurs here on large screens (and down) -->
-    <!-- Therefore, a v-spacer is added at this point -->
-    <div class="v-spacer d-xl-none"></div> <!-- d-xl-none = display: none only on extra large screens (> 1200px) -->
-    <div class="col-6 col-xl-3">
-      <div class="card">
-        <h2 class="card-title">Users</h2>
-        <h4>345</h3>
-  
-          <a href="{{url('/user')}}" class="btn btn-outline-primary d-block"> </i>Details</a>
-      </div>
-    </div>
-    <div class="col-6 col-xl-3">
-      <div class="card">
-        <h2 class="card-title">Suppliers</h2>
-        <h4>1</h3>
-          <a href="{{url('/supplier')}}" class="btn btn-outline-primary d-block"> </i>Details</a>
-      </div>
-    </div>
-  </div>
 @endsection
