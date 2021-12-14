eddd1dee0bb4b272b4e5cac8bb9cedbb17508d5c
Ashish Shukla <akshkgd@gmail.com>
2021-12-14 09:45:51 +0530

    bug fix

--- resources/views/admin/batchEnrollment.blade.php
+++ resources/views/admin/batchEnrollment.blade.php
@@ -17,21 +17,21 @@
                             </div>
 
                             <table class="table table-fluid">
-                                <thead>
+                                <tdead>
                                     <tr>
                                         <td scope="col">#</th>
                                         <td scope="col">Name</th>
                                         <td scope="col">Email</th>
                                         <td scope="col">Mobile</th>
-                                        <tr scope="col">Enrolled on </th>
-                                        <tr scope="col">Actions</th>
+                                        <td scope="col">Enrolled on </th>
+                                        <td scope="col">Actions</th>
 
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach ($paidEnrollments as $enrollment)
                                         <tr>
-                                            <th scope="row">{{ ++$i }}</th>
+                                            <td scope="row">{{ ++$i }}</th>
                                             <td> <img src="{{ $enrollment->students->avatar }}" alt=""
                                             class="avatar avatar-sm"> {{ $enrollment->students->name }}</td>
                                             <td>{{ $enrollment->students->email }}</td>
@@ -56,28 +56,24 @@
                             </div>
 
                             <table class="table table-responsive-lg">
-                                <thead>
+                                
                                     <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Email</th>
-                                        <th scope="col">Mobile</th>
-                                        <th scope="col">College</th>
-                                        <th scope="col">Course</th>
-                                        <th scope="col">Enrolled on </th>
-                                        <th scope="col">Actions</th>
+                                        <td scope="col">#</th>
+                                        <td scope="col">Name</th>
+                                        <td scope="col">Email</th>
+                                        <td scope="col">Mobile</th>
+                                        <td scope="col">Enrolled on </th>
+                                        <td scope="col">Actions</th>
                                     </tr>
-                                </thead>
+                               
                                 <tbody>
                                     @foreach ($unpaidEnrollments as $enrollment)
                                         <tr>
-                                            <th scope="row">{{ ++$i }}</th>
+                                            <td scope="row">{{ ++$i }}</th>
                                             <td> <img src="{{ $enrollment->students->avatar }}" alt=""
                                             class="avatar avatar-sm"> {{ $enrollment->students->name }}</td>
                                             <td>{{ $enrollment->students->email }}</td>
                                             <td>{{ $enrollment->students->mobile }}</td>
-                                            <td>{{ $enrollment->students->college }}</td>
-                                            <td>{{ $enrollment->students->course }}</td>
                                             <td>{{ $enrollment->created_at->format('d M')}}</td>
                                             <td>
                                                 <a href="{{action('AdminController@paymentReceived', Crypt::encrypt($enrollment->id) )}}" class="">Payment Received</a>
