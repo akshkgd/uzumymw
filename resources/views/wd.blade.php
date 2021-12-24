40090bb48de7c8d597198d419871924557140f28
Ashish Shukla <akshkgd@gmail.com>
2021-12-24 18:35:11 +0530

    fv

--- resources/views/wd.blade.php
+++ resources/views/wd.blade.php
@@ -21,13 +21,13 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}(window, document,'script',
     'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1564705437240026');
+    fbq('init', '1180474805808058');
     fbq('track', 'PageView');
-  </script>
-  <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=1564705437240026&ev=PageView&noscript=1"
-  /></noscript>
-  <!-- End Facebook Pixel Code -->
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1180474805808058&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Facebook Pixel Code -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HC9ETJV29G"></script>
     <script>
     window.dataLayer = window.dataLayer || [];
