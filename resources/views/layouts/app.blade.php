6eabd2df85aa27579c8f3e0b719bf3245080f644
Ashish Shukla <akshkgd@gmail.com>
2021-12-24 22:45:09 +0530

    fv

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,18 +13,7 @@
     <link rel="icon" href="{{asset('/assets/img/chrome-icon.png')}}">
     <meta name="facebook-domain-verification" content="nlndijpgith63pnf9skj942enj02m8" />
     <!-- Facebook Pixel Code -->
-  <script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1180474805808058');
-  fbq('track', 'PageView');
-  </script>
+
   <noscript><img height="1" width="1" style="display:none"
   src="https://www.facebook.com/tr?id=1180474805808058&ev=PageView&noscript=1"
   /></noscript>
