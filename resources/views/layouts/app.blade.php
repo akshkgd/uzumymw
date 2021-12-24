411dc13525acfe4308f30c86973b38cd0084a6d9
Ashish Shukla <akshkgd@gmail.com>
2021-12-24 16:24:27 +0530

    facebook verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,23 +12,7 @@
     <link rel="canonical" href="{{url()->current()}}"/>
     <link rel="icon" href="{{asset('/assets/img/chrome-icon.png')}}">
     <meta name="facebook-domain-verification" content="0vagnkaf1wvqul6ovwp554g4m1qeyo" />
-  <!-- Facebook Pixel Code -->
-<script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1564705437240026');
-  fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1564705437240026&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Facebook Pixel Code -->
+
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-HC9ETJV29G"></script>
     <script>
