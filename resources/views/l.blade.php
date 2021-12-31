37a8e877e3922af0920d01b1ad5abd9aa508269f
Ashish Shukla <akshkgd@gmail.com>
2022-01-01 01:06:00 +0530

    update

--- resources/views/l.blade.php
+++ resources/views/l.blade.php
@@ -14,7 +14,7 @@
             <div class="col-xl-8 col-lg-9 text-center">
                 <h2 class="display-5 mx-xl-8 ">Students love Codekaro</h2>
                 <p class="lead">
-                    Here's what {{$users}}+ satisfied students have to say about learning with codekaro.
+                    Here's what satisfied students have to say about learning with codekaro.
                 </p>
             </div>
         </div>
