bd0d142e94dd71428cd68fabab06a5329ef0b609
Ashish Shukla <akshkgd@gmail.com>
2022-01-26 16:48:30 +0530

    footer links updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -18,6 +18,8 @@
               </li>
               <li class="nav-item"><a href="{{url('/privacy')}}" class="nav-link">Privacy Policy</a>
               </li>
+              <li class="nav-item"><a href="{{url('/teach')}}" class="nav-link">Become a Teacher</a></li>
+              <li class="nav-item"><a href="https://codekaro.freshdesk.com/" class="nav-link">Raise Ticket</a></li>
               {{-- <li class="nav-item"><a href="#" class="nav-link">Elements</a> --}}
               </li>
             </ul>
