9697effc6c249857e97b0b58515ca0dd5746c8a0
Ashish Shukla <akshkgd@gmail.com>
2022-02-04 13:07:30 +0530

    wd

--- resources/views/wd.blade.php
+++ resources/views/wd.blade.php
@@ -70,10 +70,10 @@
                 </div>
 
 
-                <h4 class="fw-500 py-4"> <strong>1st February to 3rd February </strong>  | 06:00 PM IST</h3>
+                <h4 class="fw-500 py-4"> <strong>6th February to 8th February </strong>  | 06:00 PM IST</h3>
 
                     <div class="my-5">
-                        <a href="https://codekaro.in/workshop-enroll/54" class="btn btn-dark btn-lg btn-block px-5">
+                        <a href="https://codekaro.in/workshop-enroll/50" class="btn btn-dark btn-lg btn-block px-5">
                             <h2 class="fs-3 mb-1">Enroll now for free</h2>
                             <p class="mb-0 fw-200">Enroll now limited seats are available</p>
                         </a>
@@ -205,7 +205,7 @@
                   </div>
                 
                 <div class="my-5 text-center">
-                    <a href="https://codekaro.in/workshop-enroll/54" class="btn btn-dark btn-lg btn-block px-5">
+                    <a href="https://codekaro.in/workshop-enroll/50" class="btn btn-dark btn-lg btn-block px-5">
                         <h2 class="fs-3 mb-1">Enroll now for free</h2>
                         <p class="mb-0">Enroll now limited seats are available</p>
                     </a>
@@ -309,7 +309,7 @@
                 </div>
             </div>
             <div class="my-5 text-center">
-                <a href="https://codekaro.in/workshop-enroll/54" class="btn btn-dark btn-lg btn-block px-5">
+                <a href="https://codekaro.in/workshop-enroll/50" class="btn btn-dark btn-lg btn-block px-5">
                     <h2 class="fs-3 mb-1">Enroll now for free</h2>
                     <p class="mb-0">Enroll now limited seats are available</p>
                 </a>
@@ -353,7 +353,7 @@
               <p id="headline"></p>
             </div>
             <div class="my-5">
-                <a href="https://codekaro.in/workshop-enroll/54" class="btn btn-dark btn-lg btn-block px-5">
+                <a href="https://codekaro.in/workshop-enroll/50" class="btn btn-dark btn-lg btn-block px-5">
                     <h2 class="fs-3 mb-1">Enroll now for free</h2>
                     <p class="mb-0">Enroll now limited seats are available</p>
                 </a>
@@ -383,7 +383,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "feb 1, 2022 18:30:00",
+  let birthday = "feb 6, 2022 18:30:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
