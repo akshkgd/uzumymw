12d699770eadfbf0781b49910d49329547e36449
Ashish Shukla <akshkgd@gmail.com>
2022-04-13 14:23:10 +0530

    recordings updated

--- resources/views/students/recordings.blade.php
+++ resources/views/students/recordings.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
+
 @section('content')
 <style>
   .fw-400{
     font-weight: 400 !important;
+  }
+  .a{
+    color: #00bcd4;
   }
   
 </style>
@@ -28,7 +32,7 @@
     @isset($video)
         
     <div class=""></div>
-    <div id="player" class="plyr" data-plyr-provider="youtube" data-plyr-embed-id="{{$video->videoLink}}"></div>
+    <div id="player" class="plyr js-player" data-plyr-provider="youtube" data-plyr-embed-id="{{$video->videoLink}}"></div>
     <h1 class="lead-1 pt-2 pb-0" id="title">{{$video->title}}</h1>
     <div class="">
         {!!$video->desc!!}
@@ -86,9 +90,43 @@
 
 
 
-
-
 @endif
 @endisset
 
+
+
+{{-- <script src="{{asset('js/plyr.js')}}"></script> --}}
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+ // Controls (as seen below) works in such a way that as soon as you explicitly define (add) one control
+ // to the settings, ALL default controls are removed and you have to add them back in by defining those below.
+
+ // For example, let's say you just simply wanted to add 'restart' to the control bar in addition to the default.
+ // Once you specify *just* the 'restart' property below, ALL of the controls (progress bar, play, speed, etc) will be removed,
+ // meaning that you MUST specify 'play', 'progress', 'speed' and the other default controls to see them again.
+
+ const controls = [
+     'play-large', // The large play button in the center
+     'restart', // Restart playback
+     'rewind', // Rewind by the seek time (default 10 seconds)
+     'play', // Play/pause playback
+     'fast-forward', // Fast forward by the seek time (default 10 seconds)
+     'progress', // The progress bar and scrubber for playback and buffering
+     'current-time', // The current time of playback
+     'duration', // The full duration of the media
+     'mute', // Toggle mute
+     'volume', // Volume control
+     'captions', // Toggle captions
+     'settings', // Settings menu
+     'pip', // Picture-in-picture (currently Safari only)
+     'airplay', // Airplay (currently Safari only)
+     'download', // Show a download button with a link to either the current source or a custom URL you specify in your options
+     'fullscreen' // Toggle fullscreen
+ ];
+
+ const player = Plyr.setup('.js-player', { controls });
+
+});
+
+</script>
 @endsection
