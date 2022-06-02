7161cd0e02d36d440ce65ce0e3afaf89378054d4
Ashish Shukla <akshkgd@gmail.com>
2022-06-02 14:14:22 +0530

    wd

--- resources/views/wd.blade.php
+++ resources/views/wd.blade.php
@@ -76,7 +76,7 @@
                 </div>
 
 
-                <h4 class="fw-500 py-4"> <strong>1st June to 2nd June </strong>  | 06:00 PM IST</h3>
+                <h4 class="fw-500 py-4"> <strong>3rd June to 4th June </strong>  | 06:00 PM IST</h3>
 
                     <div class="my-5">
                         <a href="https://codekaro.in/workshop-enroll/101" class="btn btn-dark btn-lg btn-block px-5">
@@ -385,7 +385,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "june 1, 2022 18:00:00",
+  let birthday = "june 3, 2022 18:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
