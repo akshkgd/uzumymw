22cb7903eec33e9ae543e4b725a4f1135184cd8c
Ashish Shukla <akshkgd@gmail.com>
2022-08-15 22:21:41 +0530

    wd

--- resources/views/wd.blade.php
+++ resources/views/wd.blade.php
@@ -39,7 +39,7 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/ck_light.css')}}">
+    <link rel="stylesheet" href="{{asset('css/masterclass.css')}}">
     <style>
 
     </style>
