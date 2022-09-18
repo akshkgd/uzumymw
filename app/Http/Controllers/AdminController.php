2135e13f4f436f8fdd0fcbcb5c9dbb8315357717
Ashish Shukla <akshkgd@gmail.com>
2022-09-18 17:55:43 +0530

    email list update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
         $this->middleware(['auth', 'verified', 'isAdmin']);
     }
     public function getUsers(){
-        $users = User::select('id','name', 'email','created_at', 'updated_at')->paginate(2000);
+        $users = User::select('id','name', 'email','created_at', 'updated_at');
         
         foreach($users as $user){
             $isPaid = CourseEnrollment::where('userId', $user->id)->where('hasPaid', 1)->count();
