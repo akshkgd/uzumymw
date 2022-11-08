1bcc20de1f2309ef8a948e76d6de5dbf34c97dce
Ashish Shukla <akshkgd@gmail.com>
2022-11-08 20:22:27 +0530

    wd

--- resources/views/wd.blade.php
+++ resources/views/wd.blade.php
@@ -71,7 +71,7 @@
                             <p class="fs-6 text-muted col-md-10 text-center" style="font-size: calc(0.01rem + 1.6vw);">Modern CSS from the beginning - all the way up to Javascript with real world scenarios and examples! The must-have Frontend Bootcamp in 2022. </p>
                         </div>
         
-                <h4 class="fw-500 py-4"> <strong>Tue, 8th November to 9th November </strong>  | 06:00 PM IST</h3>
+                <h4 class="fw-500 py-4"> <strong>Friday, 11th November to 12th November </strong>  | 06:00 PM IST</h3>
                         
         
                             <div class="my-3 white">
@@ -456,7 +456,7 @@
                     <input type="number" required  class="form-control" id="floatingInput" name="mobile" placeholder="name@example.com" @auth value="{{Auth::user()->mobile}}" @endauth>
                     <label for="floatingInput">Mobile Number</label>
                   </div>
-                <input type="hidden" name="courseId" value="139">
+                <input type="hidden" name="courseId" value="140">
                 <button class="enrollment-button">Enroll Now for Free</button>
             </form>
             
@@ -480,7 +480,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "november 8, 2022 18:00:00",
+  let birthday = "november 11, 2022 18:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
