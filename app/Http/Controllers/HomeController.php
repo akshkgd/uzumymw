e6cf0e1b9b02136cb6262da5cd38177739036c42
Ashish Shukla <akshkgd@gmail.com>
2022-12-03 22:08:47 +0530

    wd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,7 +62,9 @@ class HomeController extends Controller
             $usersPreviousMonth = User::whereMonth('created_at', date('m', strtotime('-1 month')))->count();
             $batches = Batch::where('status', 1)->get()->count();
             $total = CourseEnrollment::where('hasPaid', 1)->sum('amountPaid')/100;
-            $month = CourseEnrollment::where('hasPaid', 1)->whereMonth('paidAt', $month_date)->sum('amountPaid')/100;
+            // $month = CourseEnrollment::where('hasPaid', 1)->whereMonth('paidAt', $month_date)->sum('amountPaid')/100;
+            $month = CourseEnrollment::whereMonth('created_at', '=', Carbon::now()->month)->where('hasPaid', 1)->sum('amountPaid')/100;
+
             // $previousMonth = CourseEnrollment::where('hasPaid', 1)->whereMonth('paidAt', date('m', strtotime('-1 month')))->sum('amountPaid')/100;
             $previousMonth = CourseEnrollment::whereMonth('created_at', '=', Carbon::now()->subMonth()->month)->where('hasPaid', 1)->sum('amountPaid')/100;
         return view('admin.index', compact('users', 'batches', 'total', 'month', 'previousMonth', 'usersThisMonth', 'usersPreviousMonth'));
