6eed2222bc02913af7be2218ff7f50f949d956eb
Ashish Shukla <akshkgd@gmail.com>
2022-12-07 15:47:16 +0530

    wd

--- resources/views/wd.blade.php
+++ resources/views/wd.blade.php
@@ -39,15 +39,12 @@
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('css/masterclass.css')}}">
+    <link rel="stylesheet" href="{{asset('css/wd.css')}}">
     <style>
-
+        
     </style>
-    {{-- <script>
-        if(navigator.userAgent.includes("Instagram")){
-            window.location.href = "https://codekaro.in/web-development-live-masterclass";
-        }
-     </script> --}}
+    
+    
   
 </head>
 
