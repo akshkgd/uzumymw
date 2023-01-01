effc03d3418e17cc277a20d35891519bdb01c80d
Ashish Shukla <akshkgd@gmail.com>
2023-01-01 15:56:50 +0530

    wd

--- resources/views/wd.blade.php
+++ resources/views/wd.blade.php
@@ -67,7 +67,7 @@
                             <p class="fs-6 text-muted col-md-10 text-center" style="font-size: calc(0.01rem + 1.6vw);">Modern CSS from the beginning - all the way up to Javascript with real world scenarios and examples! The must-have Frontend Bootcamp in 2022. </p>
                         </div>
         
-                <h4 class="fw-500 py-4"> <strong>Friday, 30th December to 31st December </strong>  | 06:00 PM IST</h3>
+                <h4 class="fw-500 py-4"> <strong>Tuesday, 3rd January to 4th January </strong>  | 06:00 PM IST</h3>
                         
         
                             <div class="my-3 white">
@@ -463,7 +463,7 @@
                     <input type="number" required  class="form-control" id="floatingInput" name="mobile" placeholder="name@example.com" @auth value="{{Auth::user()->mobile}}" @endauth>
                     <label for="floatingInput">Mobile Number</label>
                   </div>
-                <input type="hidden" name="courseId" value="154">
+                <input type="hidden" name="courseId" value="155">
                 <button class="enrollment-button">Enroll Now for Free</button>
                 <p style="font-size: 12px;" class="mt-3 mb-0 text-left">By registering here, I agree to Codekaro's Terms & Conditions and Privacy Policy</p>
             </form>
@@ -488,7 +488,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "december 30, 2022 18:00:00",
+  let birthday = "january 3, 2023 18:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
