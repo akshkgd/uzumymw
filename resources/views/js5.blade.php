864269784c1b3b073ab7e7f84147994bb1004c95
Ashish Shukla <akshkgd@gmail.com>
2023-06-21 01:31:08 +0530

    wd

--- resources/views/js5.blade.php
+++ resources/views/js5.blade.php
@@ -998,7 +998,7 @@
         <p class="m-0 fw-bold text-light">₹ 199  <span class="small text-white fw-light" style="text-decoration: line-through"> ₹ 899    </span></p>
         <p class="small m-0 text-light">Offer ends in <span class="text-light" id="hours2"></span> : <span class="text-light" id="minutes2"></span> : <span class="text-light" id="seconds2"></span> </p>
     </div>
-    <button data-bs-toggle="modal" data-bs-target="#enroll" class="bt button ">Join 5 days bootcamp</button>
+    <button data-bs-toggle="modal" data-bs-target="#enroll" class="bt button ">Join Bootcamp Now</button>
 </div>
 
 
