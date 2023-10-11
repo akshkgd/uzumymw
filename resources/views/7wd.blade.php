adb904339dc676c86493223df801936de01a462b
Ashish Shukla <akshkgd@gmail.com>
2023-10-11 22:01:07 +0530

    wd

--- resources/views/7wd.blade.php
+++ resources/views/7wd.blade.php
@@ -397,7 +397,7 @@
                                 <div class="card-dark">
                                     <div class="  text-white fs-6 d-flex align-items-center" style="gap:12px">
                                         <img src="{{ asset('assets/img/l3.svg') }}" alt="">
-                                        From 11th October to 15th October
+                                        From 25th October to 29th October
                                     </div>
                                 </div>
 
@@ -1178,7 +1178,7 @@
                         <p class="">Three months access of recordings. Only available if you attend. This is <strong>EARLY
                                 Bird</strong> offer, It will increase to 499 once the timer hits zero.
                         </p>
-                        <input type="hidden" name="courseId" value="54">
+                        <input type="hidden" name="courseId" value="56">
                         <input type="hidden" id="recordingsCheckbox" name="recordingsCheckbox" value="0">
                         <button type="submit" class="enrollment-button d-flex align-items-center justify-content-center"
                             onclick="startLoader()">Join bootcamp now at Rs.<span class="text-white"
