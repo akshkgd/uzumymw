c8f729ea5cd11cc67dae7be2565f3abb3084fe48
Ashish Shukla <akshkgd@gmail.com>
2023-11-06 20:58:24 +0530

    wd

--- resources/views/students/recordings.blade.php
+++ resources/views/students/recordings.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.player')
 <link rel="stylesheet" href="{{ asset('css/ck-plyr.css') }}">
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 
 
 @section('content')
     <style>
+        /* .plyr__controls .plyr__controls__item[data-plyr="fast-forward"] {
+    background-image: url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="67" height="67" fill="none" viewBox="0 0 67 67"%3E%3Cpath fill="%23fff" fill-rule="evenodd" d="M46.332 5.773a4.125 4.125 0 0 0-4.125 4.125v46.75a4.127 4.127 0 0 0 4.125 4.125 4.127 4.127 0 0 0 4.125-4.125V9.898a4.125 4.125 0 0 0-4.125-4.125zM25.707 9.898v46.75a4.125 4.125 0 1 1-8.25 0V9.898a4.123 4.123 0 0 1 4.125-4.125 4.123 4.123 0 0 1 4.125 4.125z" clip-rule="evenodd"%3E%3C/svg%3E');
+    background-size: contain; /* Adjust as needed */
+    /* Additional styles as needed */
+}
+
+
+    .plyr__controls .plyr__controls__item[data-plyr="rewind"] {
+        background-image: url('path/to-your-custom-rewind-icon.svg');
+    } */
         .ytp-chrome-top-buttons{
                 display:none;
             }
@@ -36,14 +46,131 @@
                 background: #2d0069 !important;
                 color: #fff;
         }
+        .video-container {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-bottom: 56.25%; /* Aspect ratio 16:9 */
+            overflow: hidden;
+            transition: opacity 0.3s;
+        }
+        .player-ea4b66e4-85a5-49b9-9193-f022dc194687{
+            background: black !important;
+        }
+        .video-container:hover {
+            opacity: 1;
+        }
+
+        #vimeoPlayer {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+
+        .custom-controls {
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            width: 100%;
+            height: 20%;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            transition: opacity 0.3s;
+            opacity: 0;
+            z-index: 1; /* Ensure controls are on top */
+        }
+
+        .video-container:hover .custom-controls {
+            opacity: 1;
+        }
+
+        .custom-controls button {
+            background: transparent;
+            border: none;
+            cursor: pointer;
+            font-size: 34px;
+            color: #b7b7b7c3; 
+            transition: color 0.3s;
+        }
+
+        .custom-controls button:hover {
+            color: #ffffff;
+            cursor: pointer;
+        }
+
+        .custom-controls button i {
+            display: block;
+        }
+        .video-container {
+            position: relative;
+            width: 100%;
+            height: 0;
+            padding-bottom: 56.25%; /* Aspect ratio 16:9 */
+            overflow: hidden;
+            transition: opacity 0.3s;
+        }
+        .player-ea4b66e4-85a5-49b9-9193-f022dc194687{
+            background: black !important;
+        }
+        .video-container:hover {
+            opacity: 1;
+        }
+
+        #vimeoPlayer {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+
+        .custom-controls {
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            width: 100%;
+            height: 20%;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            transition: opacity 0.3s;
+            opacity: 0;
+            z-index: 1; /* Ensure controls are on top */
+        }
+
+        .video-container:hover .custom-controls {
+            opacity: 1;
+        }
+
+        .custom-controls button {
+            background: transparent;
+            border: none;
+            cursor: pointer;
+            font-size: 34px;
+            color: #b7b7b7c3; 
+            transition: color 0.3s;
+        }
+
+        .custom-controls button:hover {
+            color: #d5d5d5c3; 
+            cursor: pointer;
+        }
+
+        .custom-controls button i {
+            display: block;
+        }
     </style>
 
     
-  <nav class="navbar p-5 shadow" data-overlay>
-    {{-- <h3 class="intro-title">Web dev starter bootcamp</h3> --}}
-    <a href=""><img src="{{asset('assets/img/color-1.svg')}}" height="34" alt=""></a>
+  {{-- <nav class="navbar p-5 shadow" data-overlay>
+    <a href="" class="logo" style=""><img src="{{asset('assets/img/color-1.svg')}}" height="30" alt=""> </a>
     <a href="{{url('/home')}}" class="btn">Back to dashboard</a>
-</nav>
+</nav> --}}
 
 
 
@@ -61,6 +188,11 @@
             <div class="intro">
                 <h2 class="intro-title">{{$enrollment->batch->name}}</h2>
                 <span class="intro-desc">{{ $content->where('type', 2)->count() }} Videos <span class="text-xs">({{ $content->where('type', 1)->count() }} Assignments)</span></span>
+                <div>
+                    <a href="{{url('/home')}}" class="back"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                      </svg>  Back to dashboard</a>
+                </div>
             </div>
                 @foreach ($content as $c)
                 <a id="item" class="{{Str::contains($c->id, $video->id) ? 'active' : ' '}}"  href="{{ route('recordings', ['batchId' => $batchId, 'cId' => Crypt::encrypt($c->id)]) }}">
@@ -84,8 +216,61 @@
         <div id="js-player" class="js-player" data-plyr-provider="youtube"
             data-plyr-embed-id="{{ $video->videoLink }}"></div>
         @else
-        <div style="padding:50% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/{{$video->videoLink}}?autoplay=1&badge=0&amp;autopause=0&amp;quality_selector=1&amp;progress_bar=1&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Recording   CSS Media Queries - 651d75bfe4b0e4a748954b62 (1)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-
+        {{-- <div style="padding:50% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/{{$video->videoLink}}?autoplay=1&badge=0&amp;autopause=0&amp;quality_selector=1&amp;progress_bar=1&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Recording   CSS Media Queries - 651d75bfe4b0e4a748954b62 (1)"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script> --}}
+        <div class="video-container">
+            <iframe id="vimeoPlayer" src="https://player.vimeo.com/video/{{$video->videoLink}}?autoplay=1&badge=0&amp;autopause=0&amp;quality=720p" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" title="Recording CSS Media Queries - 651d75bfe4b0e4a748954b62 (1)"></iframe>
+            <div class="custom-controls">
+                <button id="fastBackwardButton" class="icon-backward"><i class="bi bi-arrow-counterclockwise"></i></button>
+                <button id="playPauseButton" class="icon-play"><i class="bi bi-play"></i></button>
+                <button id="fastForwardButton" class="icon-forward"><i class="bi bi-arrow-clockwise"></i></button>
+            </div>
+        </div>
+    
+        <script src="https://player.vimeo.com/api/player.js"></script>
+        <script>
+            const iframe = document.getElementById('vimeoPlayer');
+            const player = new Vimeo.Player(iframe);
+            let isPlaying = false;
+        
+            function updatePlayButton() {
+                const playPauseButton = document.getElementById('playPauseButton');
+                if (isPlaying) {
+                    playPauseButton.innerHTML = '<i class="bi bi-pause"></i>';
+                } else {
+                    playPauseButton.innerHTML = '<i class="bi bi-play"></i>';
+                }
+            }
+        
+            document.getElementById('fastBackwardButton').addEventListener('click', function() {
+                player.getCurrentTime().then(function(seconds) {
+                    player.setCurrentTime(seconds - 10);
+                });
+            });
+        
+            document.getElementById('playPauseButton').addEventListener('click', function() {
+                if (isPlaying) {
+                    player.pause();
+                } else {
+                    player.play();
+                }
+            });
+        
+            document.getElementById('fastForwardButton').addEventListener('click', function() {
+                player.getCurrentTime().then(function(seconds) {
+                    player.setCurrentTime(seconds + 10);
+                });
+            });
+        
+            player.on('play', function() {
+                isPlaying = true;
+                updatePlayButton();
+            });
+        
+            player.on('pause', function() {
+                isPlaying = false;
+                updatePlayButton();
+            });
+        </script>
         @endif
         
     @endif
