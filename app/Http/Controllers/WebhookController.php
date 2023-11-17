bb291834b6b3bbef471978353800ea5f91de6b66
Ashish Shukla <akshkgd@gmail.com>
2023-11-17 12:24:49 +0530

    webhook update

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -25,47 +25,61 @@ class WebhookController extends Controller
 {
 
     public function grantAccess(Request $request)
-{   
-    \Log::info('Webhook Request:', $request->all());
-    $payload = $request->input('payload');
-    $payment = $payload['payment'] ?? null;
-    $entity = $payment['entity'] ?? null;
-    $notes = $entity['notes'] ?? null;
+    {
+        \Log::info('Webhook Request:', $request->all());
+        $payload = $request->input('payload');
+        $payment = $payload['payment'] ?? null;
+        $entity = $payment['entity'] ?? null;
+        $notes = $entity['notes'] ?? null;
 
-    if ($notes) {
-        $enrollment = CourseEnrollment::where('userId', $notes['UserId'])
-                                     ->where('batchId', $notes['CourseId'])
-                                     ->first();
+        // if ($notes) {
+        //     $enrollment = CourseEnrollment::where('userId', $notes['UserId'])
+        //                                  ->where('batchId', $notes['CourseId'])
+        //                                  ->first();
 
-        if ($enrollment && $enrollment->hasPaid == 0) {
-            $enrollment->status = 1;
-            $enrollment->hasPaid = 1;
-            $enrollment->amountPaid = $entity['amount'];
-            $enrollment->paidAt = Carbon::now();
-            $enrollment->paymentMethod = $entity['method'];
-            $enrollment->transactionId = $entity['id'];
+        //     if ($enrollment && $enrollment->hasPaid == 0) {
+        //         $enrollment->status = 1;
+        //         $enrollment->hasPaid = 1;
+        //         $enrollment->amountPaid = $entity['amount'];
+        //         $enrollment->paidAt = Carbon::now();
+        //         $enrollment->paymentMethod = $entity['method'];
+        //         $enrollment->transactionId = $entity['id'];
 
-            // Add a comment to field2 indicating the webhook data update
-            $enrollment->field2 = 'webhook access granted';
-            $enrollment->save();
+        //         Add a comment to field2 indicating the webhook data update
+        //         $enrollment->field2 = 'webhook access granted';
+        //         $enrollment->save();
 
-            return response('Webhook Handled', 200);
-        } else {
-            // Add a comment or additional handling logic for cases when the enrollment has already been paid
-            // ...
-            if ($enrollment) {
-                $enrollment->field2 = 'webhook called!!';
+        //         return response('Webhook Handled', 200);
+        //     } 
+        if ($notes) {
+            $enrollment = CourseEnrollment::where('userId', $notes['UserId'])
+                ->where('batchId', $notes['CourseId'])
+                ->first();
+
+            if ($enrollment && $enrollment->hasPaid == 0) {
+                $enrollment->status = 1;
+                $enrollment->hasPaid = 1;
+                $enrollment->amountPaid = $entity['amount'];
+                $enrollment->paidAt = Carbon::now();
+                $enrollment->paymentMethod = $entity['method'];
+                $enrollment->transactionId = $entity['id'];
+
+                // Add a comment to field2 indicating the webhook data update
+                $enrollment->field2 = 'webhook access granted';
                 $enrollment->save();
+                return response('Webhook Handled', 200);
+            } else {
+                // Add a comment or additional handling logic for cases when the enrollment has already been paid
+                // ...
+                if ($enrollment) {
+                    $enrollment->field2 = 'webhook called!!';
+                    $enrollment->save();
+                }
+                return response('Webhook Handled', 200);
             }
+        } else {
+            \Log::error('Payment notes not found in webhook request.');
             return response('Webhook Handled', 200);
         }
-    } else {
-        \Log::error('Payment notes not found in webhook request.');
-        return response('Webhook Handled', 200);
     }
-}
-
-
-
-
 }
