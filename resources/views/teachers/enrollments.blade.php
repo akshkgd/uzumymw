ecb049b67d278f0d18a327d8950d6d5e31aeccce
Ashish Shukla <akshkgd@gmail.com>
2023-12-27 15:25:34 +0530

    wd

--- resources/views/teachers/enrollments.blade.php
+++ resources/views/teachers/enrollments.blade.php
@@ -41,7 +41,7 @@
                                 <td><a href="tel:+{{$enrollment->students->mobile}}">{{$enrollment->students->mobile}}</a></td>
                                 <td>{{$enrollment->students->created_at->format('d M y')}}</td>
                                 <td>{{ \Carbon\Carbon::parse($enrollment->paidAt)->format('d M y') }}  </td>
-                                <td>{{$enrollment->students->field3}}</td>
+                                <td>{{$enrollment->students->field1}}</td>
                                 <td>
                                     @if ($enrollment->certificateId =='')
                                     <a href="{{action('TeacherController@generateCertificate', $enrollment->id )}}" class="card-link">generate certificate</a>
