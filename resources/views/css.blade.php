bef49122ce94a5e69e01c3c37913f9b6479646c4
Ashish Shukla <akshkgd@gmail.com>
2024-01-21 23:31:08 +0530

    wd

--- resources/views/css.blade.php
+++ resources/views/css.blade.php
@@ -2115,7 +2115,13 @@ iframe{border:0;}
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#29cf84" class="bi bi-check-all" viewBox="0 0 16 16">
                                         <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z"/>
                                       </svg>
-                                      <span>1 Private zoom call(Career Guidance) from Ashish</span>
+                                      <span>1 Private zoom coaching call</span>
+                                </div>
+                                <div class="d-flex align-items-center mb-2" style="gap: 8px">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#29cf84" class="bi bi-check-all" viewBox="0 0 16 16">
+                                        <path d="M8.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L2.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093L8.95 4.992zm-.92 5.14.92.92a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 1 0-1.091-1.028L9.477 9.417l-.485-.486z"/>
+                                      </svg>
+                                      <span>Career guidance from Ashish</span>
                                 </div>
                                 <div class="d-flex align-items-center mb-2" style="gap: 8px">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#29cf84" class="bi bi-check-all" viewBox="0 0 16 16">
