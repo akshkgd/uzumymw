26061535fb3cc8e9b1bd7443f1bfc6655af1ad40
Ashish Shukla <akshkgd@gmail.com>
2024-04-18 23:56:56 +0530

    webhook update

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -32,31 +32,11 @@ class WebhookController extends Controller
     $payment = $payload['payment'] ?? null;
     $entity = $payment['entity'] ?? null;
     $notes = $entity['notes'] ?? null;
-    // checking for frontend or full stack course
-    if($entity['amount'] > 1000000){
-                    $user = User::where('email', $entity['email'])->first();
-                    $a = new CourseEnrollment;
-                    $a->userId = $user->id;
-                    $a->batchId = ($entity['amount'] == 1099900) ? 68 : (($entity['amount'] == 1799800) ? 67 : 0);
-                    $a->price = 24999;
-                    $a->amountpayable = 24999;
-                    $a->amountPaid = $entity['amount'];
-                    $a->paidAt = Carbon::now();
-                    $a->paymentMethod = $entity['method'];
-                    $a->transactionId = $request->$entity['id'];
-                    $a->invoiceId = $request->$entity['id'];
-                    $a->status = 1;
-                    $a->hasPaid =1;
-                    $a->certificateId = substr(md5(time()), 0, 16);
-                    $a->save();            
-    }
-    else{
     if ($notes) {
-        // $enrollment = CourseEnrollment::where('userId', $notes['UserId'])
-        //                              ->where('batchId', $notes['CourseId'])
-        //                              ->first();
-
-        $enrollment = CourseEnrollment::findorFail($notes['enrollmentId']);
+        $enrollment = CourseEnrollment::where('userId', $notes['UserId'])
+                                     ->where('batchId', $notes['CourseId'])
+                                     ->first();
+        // $enrollment = CourseEnrollment::findorFail($notes['enrollmentId']);
         $sendPabbly = $this->sendPabbly($enrollment->id, $entity['amount']);
         if ($enrollment && $enrollment->hasPaid == 0) {
             $enrollment->status = 1;
@@ -89,7 +69,7 @@ class WebhookController extends Controller
         \Log::error('Payment notes not found in webhook request.');
         return response('Webhook Handled', 200);
     }
-    }
+    
 }
 
 private function sendPabbly($id, $p){
