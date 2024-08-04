0011f00a8ed22e6a6e68230c57474a386808f158
Ashish Shukla <akshkgd@gmail.com>
2024-08-04 23:01:59 +0530

    wd

--- resources/views/students/editProfile.blade.php
+++ resources/views/students/editProfile.blade.php
@@ -10,16 +10,16 @@
     >
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         @if(session('alert-success'))
-    <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-        <strong class="font-bold">Success!</strong>
-        <span class="block sm:inline">{{ session('alert-success') }}</span>
+    <div class="border-green-400 text-green-600 px-4" role="alert">
+        
+        <span class="block text-small">{{ session('alert-success') }}</span>
     </div>
 @endif
 
 @if(session('alert-danger'))
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+    <div class=" text-red-700 px-4 py-3 rounded relative" role="alert">
         <strong class="font-bold">Error!</strong>
-        <span class="block sm:inline">{{ session('alert-danger') }}</span>
+        <span class="block sm:inline text-small">{{ session('alert-danger') }}</span>
     </div>
 @endif
         <h2 class="text-2xl font-bold leading-9 tracking-tight text-gray-900">
