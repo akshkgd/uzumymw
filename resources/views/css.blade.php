ae42a92081b8a987687646e95f04f80d47cde627
Ashish Shukla <akshkgd@gmail.com>
2024-08-15 01:19:27 +0530

    wd

--- resources/views/css.blade.php
+++ resources/views/css.blade.php
@@ -1341,7 +1341,7 @@ iframe{border:0;}
                                 <div class="card-dark">
                                     <div class="  text-white fs-6 d-flex align-items-center" style="gap:12px">
                                         <img src="{{ asset('assets/img/l3.svg') }}" alt="">
-                                        From 12th August
+                                        From 21st August
                                     </div>
                                 </div>
 
@@ -2179,7 +2179,7 @@ iframe{border:0;}
                         <p class="d-none">Three months access of recordings. Only available if you attend. This is <strong>EARLY
                                 Bird</strong> offer, It will increase to 499 once the timer hits zero.
                         </p>
-                        <input type="hidden" name="courseId" value="89">
+                        <input type="hidden" name="courseId" value="90">
                         <input type="hidden" id="recordingsCheckbox" name="recordingsCheckbox" value="0">
                         <button type="submit" class="enrollment-button d-flex align-items-center justify-content-center"
                             onclick="startLoader()">Join bootcamp now at Rs.<span class="text-white"
