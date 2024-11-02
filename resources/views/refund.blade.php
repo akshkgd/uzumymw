cd03964ce23c6c5b9653c11296ce57f0ac3153cf
Ashish Shukla <akshkgd@gmail.com>
2024-11-02 21:44:29 +0530

    wd

--- resources/views/refund.blade.php
+++ resources/views/refund.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.ck')
 @section('content')
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-    <div class="container ">
-     
-      <a class="navbar-brand fw-bold text-primary fs-4" href="{{url('/')}}">Codekaro</a>
+    <div class="container">
+        <a class="navbar-brand fw-bold text-primary fs-4" href="{{ url('/') }}">Codekaro</a>
     </div>
 </nav>
 
 <div class="container mt-5 pt-5">
-
-<h2 style="text-align: center;"><b>Refund & Cancellation Policy</b></h2>
-<p>Last updated: 2021-07-20</p>
-<p>There is a strict no refund & no cancellation policy. You are entitled to a refund only in the case where you have not been allotted the course after payment.</p>
+    <h2 style="text-align: center;"><b>Refund & Cancellation Policy</b></h2>
+    <p>Last updated: 2021-07-20</p>
+    <p>There is a strict no refund & no cancellation policy. You are entitled to a refund only in the case where you have not been allotted the course after payment.</p>
+    <p>If you initiate multiple subscriptions for the same course, you can cancel the additional subscriptions at any moment. However, any payments made will not be refunded, and you will retain course access for those additional months.</p>
 </div>
-
 
 {{-- <div class="d-flex">
     @include('layouts.ck-footer')
