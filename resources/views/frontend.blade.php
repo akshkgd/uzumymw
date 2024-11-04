2ab3b851dc1265e8cf2a6774d679038e9e853c2a
Ashish Shukla <akshkgd@gmail.com>
2024-11-04 21:46:25 +0530

    wd

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -32,7 +32,7 @@
              <div class="flex flex-col md:flex-row justify-center gap-4 mt-10">
                 <div class="inline-block  bg-neutral-800 py-2 px-4 sm:text-2xl text-xl font-medium text-neutral-200 rounded-lg ring-4 ring-neutral-800">From 10th November</div>
                 <div class="inline-block  bg-neutral-800 py-2 px-4 sm:text-2xl text-xl font-medium text-neutral-200 rounded-lg ring-4 ring-neutral-800">8 Weeks Cohort</div>
-                <div class="inline-block  bg-neutral-800 py-2 px-4 sm:text-2xl text-xl font-medium text-neutral-200 rounded-lg ring-4 ring-neutral-800">Only Rs 3999/-</div>
+                <div class="inline-block  bg-neutral-800 py-2 px-4 sm:text-2xl text-xl font-medium text-neutral-200 rounded-lg ring-4 ring-neutral-800">Only Rs 29999/-</div>
             
              </div>
              {{-- <img src="../fr-placed.svg" alt="" class="inline-block w-96 mt-10 text-center"> --}}
@@ -305,11 +305,13 @@
                 }, 1000);
               </script>
               @auth
-              <a href="https://codekaro.in/enroll/97" class="inline-block text-white bg-gradient-to-r from-green-600 to-green-500 bg-clip-tex mt-3 mb-12 sm:px-12 w-full sm:w-auto text-center py-4 sm:rounded-xl rounded-full sm:text-3xl text-xl font-bold shado shado-xl shadow-neutral-500">Join frontend cohort now at 3999/-</a>
+              <a href="https://codekaro.in/enroll/97" class="inline-block text-white bg-gradient-to-r from-green-600 to-green-500 bg-clip-tex mt-3 mb-12 sm:px-12 w-full sm:w-auto text-center py-4 sm:rounded-xl rounded-full sm:text-3xl text-xl font-bold shado shado-xl shadow-neutral-500">Join frontend cohort now at 29999/-</a>
                 
               @endauth
               @guest
-              <a href="https://rzp.io/rzp/frontend-cohort" class="inline-block text-white bg-gradient-to-r from-green-600 to-green-500 bg-clip-tex mt-3 mb-12 sm:px-12 w-full sm:w-auto text-center py-4 sm:rounded-xl rounded-full sm:text-3xl text-xl font-bold shado shado-xl shadow-neutral-500">Join frontend cohort now at 3999/-</a>
+              <a href="https://codekaro.in/enroll/97" class="inline-block text-white bg-gradient-to-r from-green-600 to-green-500 bg-clip-tex mt-3 mb-12 sm:px-12 w-full sm:w-auto text-center py-4 sm:rounded-xl rounded-full sm:text-3xl text-xl font-bold shado shado-xl shadow-neutral-500">Join frontend cohort now at 29999/-</a>
+
+              {{-- <a href="https://rzp.io/rzp/frontend-cohort" class="inline-block text-white bg-gradient-to-r from-green-600 to-green-500 bg-clip-tex mt-3 mb-12 sm:px-12 w-full sm:w-auto text-center py-4 sm:rounded-xl rounded-full sm:text-3xl text-xl font-bold shado shado-xl shadow-neutral-500">Join frontend cohort now at 29999/-</a> --}}
               @endguest
         </div>
      </section>
