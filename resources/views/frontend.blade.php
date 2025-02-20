0d4d193ce0f7ab3d4cfecbf5e6811b9092cbd00c
Ashish Shukla <akshkgd@gmail.com>
2025-02-20 13:48:25 +0530

    wd

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="output.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/collapse@3.x.x/dist/cdn.min.js"></script>
  
 <!-- Alpine Core -->
