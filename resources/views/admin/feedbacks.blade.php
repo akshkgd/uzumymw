9be76ed378fcb009de8afa1df81f6ef9a8d477ab
Ashish Shukla <akshkgd@gmail.com>
2025-03-14 00:10:09 +0530

    wd

--- resources/views/admin/feedbacks.blade.php
+++ resources/views/admin/feedbacks.blade.php
@@ -23,6 +23,7 @@
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
                 @foreach($feedbacks as $feedback)
+                @if($feedback->status == 1)
                 <tr class="hover:bg-neutral-50 transition-all duration-300" id="feedback-row-{{ $feedback->id }}">
                     <td class="px-6 py-3 whitespace-nowrap">
                         <div class="flex items-center ">
@@ -69,6 +70,7 @@
                         </div>
                     </td>
                 </tr>
+                @endif
                 @endforeach
             </tbody>
         </table>
