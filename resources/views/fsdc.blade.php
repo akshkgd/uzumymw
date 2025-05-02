ec6683df71ccb02ab1dd9ec94bc20e2406dcb71f
Ashish Shukla <akshkgd@gmail.com>
2025-05-02 18:35:25 +0530

    wd

--- resources/views/fsdc.blade.php
+++ resources/views/fsdc.blade.php
@@ -147,7 +147,7 @@
                     <div class="mt-8 flex flex-row items-center justify-center gap-4">
                         <a href="https://rzp.io/rzp/fullstack-cohort" class="group bg-black py-4 px-6 text-white rounded-xl border border-black hover:text-white transition-all flex items-center gap-2">
                             Join the Fullstack Cohort Now for ₹7,999
-                            <span class="transform transition-transform group-hover:translate-x-2">
+                            <span class="transform transition-transform group-hover:translate-x-2 hidden sm:block">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
                                 </svg>
