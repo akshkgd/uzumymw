281b4aad1ffa9d74a1bb4a277a602137832aff6d
Ashish Shukla <akshkgd@gmail.com>
2025-06-03 18:59:21 +0530

    wd

--- resources/views/css.blade.php
+++ resources/views/css.blade.php
@@ -44,33 +44,19 @@
   /></noscript>
   <!-- End Meta Pixel Code -->
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-HC9ETJV29G"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-HC9ETJV29G');
-    </script> --}}
-
-    <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-K8B392D');</script>
-  <!-- End Google Tag Manager -->
-  <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11206387820"></script>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9323KT1W2S"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'AW-11206387820');
-</script><script type="text/javascript">
-    window.addEventListener("load", function () {    document.querySelector('body').classList.add('loaded');  });
-  </script>
+  gtag('config', 'G-9323KT1W2S');
+</script>
+    
+    <script type="text/javascript">
+        window.addEventListener("load", function () {    document.querySelector('body').classList.add('loaded');  });
+    </script>
   {{-- <link rel="stylesheet" href="{{asset('assets/css/ck.css')}}" /> --}}
   <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
   
