28302914c3b6b247472aab8b5d5993f41c191ecd
Ashish Shukla <akshkgd@gmail.com>
2025-08-20 00:08:52 +0530

    wd

--- resources/views/banned.blade.php
+++ resources/views/banned.blade.php
@@ -6,7 +6,7 @@
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         </div>
     
-        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <div class="sm:mx-auto sm:w-full sm:max-w-xl">
           <div class="mt-1 flex justify-center">
             <div class="inline-flex w-64 items-center justify-center ">
               
@@ -14,8 +14,8 @@
             </div>
           </div>
           <h2 class="text-center text-xl mt-2 font-semibold leading-9 tracking-tigh text-gray-900">Your account has been Terminated!</h2>
-          <p class="bg-white px-6 text-neutral-500 text-center">You are currently logged in from <span class="font-bold">{{Auth::user()->email}}</span></p>
-          <p class="bg-white px-6 text-neutral-500 text-center">If you think this is an error, please contact support.</p>
+          <p class="bg-white px-6 text-neutral-700 text-center">You are currently logged in from <span class="font-bold">{{Auth::user()->email}}</span></p>
+          <p class="bg-white px-6 text-neutral-700 text-center">If you think this is an error, please contact support.</p>
           <!-- <div class="text-center">
             <a href="" class="border px-5 py-3 rounded-lg inline-block mt-6">Back to Dashboard</a>
           </div> -->
