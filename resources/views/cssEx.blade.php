2c7600b74d9d500edf428a7b9773b9ba39f30688
Ashish Shukla <akshkgd@gmail.com>
2025-11-28 00:56:00 +0530

    wd

--- resources/views/cssEx.blade.php
+++ resources/views/cssEx.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gen AI Engineering Mastermind</title>
+    <title>CSS Mastermind</title>
 
     <!-- Open Graph Meta Tags -->
     <meta property="og:title" content="CSS Mastermind">
